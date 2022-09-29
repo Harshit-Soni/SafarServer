41736cf641acc1aabcc5e5f333a36d70226665f7
Ritika Gupta <guptaritika782@gmail.com>
2022-09-29 20:49:43 +0530

    credentials updated

--- src/db.ts
+++ src/db.ts
@@ -1,8 +1,8 @@
 import { Client } from "pg";
+
 //or native libpq bindings
 //var pg = require('pg').native
 
-const conString =
-  "postgres://svwwlfcs:JDMqqOht97BpfnaAUvXYbpbQAx5c2-bd@tiny.db.elephantsql.com/svwwlfcs"; //Can be found in the Details page
+const conString = "*"; //Can be found in the Details page
 export const client = new Client(conString);
 client.connect();
